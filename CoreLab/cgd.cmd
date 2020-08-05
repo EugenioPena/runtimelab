3bfdcf5388b1528d481461d96fe4da6370415fb5
Andrew Au <andrewau@microsoft.com>
2020-08-05 11:50:29 -0700

    Sharing my local development environment

---
+++ CoreLab/cgd.cmd
@@ -0,0 +1,4 @@
+set andrew_debug=yes
+call %~dp0..\dotnet.cmd %~dp0..\artifacts\bin\coreclr\windows.x64.Debug\crossgen2\crossgen2.dll @%~dp0..\CoreLab\c.rsp
+set andrew_debug=
+
---
+++ CoreLab/cgr.cmd
@@ -0,0 +1,1 @@
+%~dp0..\dotnet.cmd %~dp0..\artifacts\bin\coreclr\windows.x64.Debug\crossgen2\crossgen2.dll @%~dp0..\CoreLab\c.rsp
